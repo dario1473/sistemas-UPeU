33b9428b40c0a30f5ca2842ad8c45bfc4fed3979
Alumnos <Alumnos@LC01-PC18>
2016-01-14 12:24:24 -0500

    subi clase 2

---
+++ Dark/src/pe/edu/upeu/poo/clase2/Repaso.java
@@ -0,0 +1,56 @@
+package pe.edu.upeu.poo.clase2;
+
+import java.awt.image.DataBufferUShort;
+
+public class Repaso {
+
+	public static void main(String[] args) {
+		
+		Repaso r= new Repaso();
+		
+		
+		 double a=10;
+		 double b=15;		
+		
+		/**
+		 *suma +
+		 *resta -
+		 *multiplicacion *
+		 *divicion /
+		 *modulo %
+		 *
+		 *
+		 */
+		
+		 
+		
+		System.out.print(Math.sqrt(a+b));
+		
+		
+		
+		System.out.println("operadores matematicos");
+		
+		/**
+		 * and&&
+		 * or ||
+		 * negacion !
+		 */
+		
+		
+		System.out.println(!false);
+		
+
+	}
+	 double peso=16;
+	 double altura=15;	
+	 Repaso r= new Repaso();
+	 calcule = r.
+	
+	     public double calcule(double peso,double altura){
+	     double imc = peso/Math.pow(altura, 2);
+	     return  imc;
+	
+	
+        }
+	
+}
