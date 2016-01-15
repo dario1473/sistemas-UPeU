8f57951dc0cf1f79ec7ad998cd4c6a5d45e721ed
Alumnos <Alumnos@LC01-PC18>
2016-01-15 11:50:07 -0500

    se subio pratica 1

---
+++ Dark/src/Practica/Bonif.java
@@ -0,0 +1,33 @@
+package Practica;
+
+import java.util.Scanner;
+
+
+
+public class Bonif {
+  
+	public static void main(String[] args) {
+
+		Scanner Bonif = new Scanner(System.in);
+	     double lol = 0;
+	    System.out.println("ingrese el monto");
+	     double m = Bonif.nextDouble();	
+	     
+	     if (m>0 && m<1000);
+	        System.out.println("No recibe bonificacion");
+	        else if (m>=1000 && m<5000);
+	         lol = m*3/100 ;
+	    
+	  
+	  
+	  
+	  
+	  
+	  
+	
+	  
+	} 
+	  
+}
+
+
